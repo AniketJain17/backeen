4c02f574af2e3735d6f126daf5fc701cf74906e1
Aniket Jain <aniket.jain2020@uem.edu.in>
2024-07-31 09:41:02 +0530

    vercel

--- index.js
+++ index.js
@@ -13,7 +13,19 @@ const errorHandler = require('./src/middlewares/errorHandler')
 const app = express()
 dotenv.config()
 
-app.use(cors())
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
+  
+  app.options('*', (req, res) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.send();
+  });
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
